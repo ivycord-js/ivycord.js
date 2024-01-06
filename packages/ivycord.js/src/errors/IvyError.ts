fb1c9685691ef2a60e58aefc3259b47a4bd142dc
Faris <41439673+null68@users.noreply.github.com>
2024-01-06 19:13:42 +0100

    Update IvyError.ts
    Co-authored-by: Jovan Jovanovic <jovanovicjovan921@gmail.com>

--- packages/ivycord.js/src/errors/IvyError.ts
+++ packages/ivycord.js/src/errors/IvyError.ts
@@ -1,16 +1,13 @@
-type Errors =
-  | 'WS_ALREADY_CONNECTED'
-  | 'LARGE_THRESHOLD_INVALID'
-  | 'FETCH_ERROR';
-
-const messages: Record<Errors, string> = {
+const messages = {
   WS_ALREADY_CONNECTED: 'Already connected to the WebSocket',
   LARGE_THRESHOLD_INVALID: 'Large threshold must be between 50 and 250',
   FETCH_ERROR: 'An error occurred while fetching\n\n{message}'
 };
 
+type ErrorsKey = keyof typeof messages;
+
 class IvyError extends Error {
-  constructor(error: Errors, message?: string) {
+  constructor(error: ErrorsKey, message?: string) {
     const errorMessage = messages[error].replace(/{message}/g, message ?? '');
     super(errorMessage);
   }
