a861cfb0ec473453dce92ca96bb2c37b4be5a868
null68 <fariscomaga@gmail.com>
2024-01-08 01:41:54 +0100

    Update BaseClient and Shard classes

--- examples/test.js
+++ examples/test.js
@@ -1,16 +1,16 @@
 const { BaseClient } = require('../packages/ivycord.js/dist/index');
 
 const client = new BaseClient({
-  token: 'YOUR_TOKEN',
+  token:
+    'MTE1MzA1MDg2NjkzMzMwOTUwMA.GKn0YM.5nlA4n2Q6kICBPIe-kjk5Vt2yxj-CCHf2n5keY',
   compress: true
 });
-
 client.on('heartbeat', () => {
   console.log('heartbeat received');
 });
 
 client.on('ready', () => {
-  console.log('ready');
+  console.log('ready, latency ' + client.shard.latency);
 });
 
 client.connect();
--- packages/ivycord.js/src/gateway/Shard.ts
+++ packages/ivycord.js/src/gateway/Shard.ts
@@ -1,6 +1,10 @@
 import { RawData, WebSocket } from 'ws';
-import { GatewayOpcodes, GatewayVersion } from 'discord-api-types/gateway/v10';
-import { Inflate, Z_SYNC_FLUSH } from 'zlib-sync';
+import { Z_SYNC_FLUSH, Inflate } from 'zlib-sync';
+import {
+  GatewayCloseCodes,
+  GatewayOpcodes,
+  GatewayVersion
+} from 'discord-api-types/gateway/v10';
 
 import { BaseClient } from '../core/BaseClient';
 import { IvyError } from '../utils/errors/IvyError';
@@ -9,11 +13,13 @@ type ConnectionStatus =
   | 'DISCONNECTED'
   | 'HANDSHAKING'
   | 'CONNECTED'
-  | 'RECONNECTING';
+  | 'RECONNECTING'
+  | 'RESUMING';
 
 class Shard {
   public client: BaseClient;
   public ws: WebSocket;
+  public latency: number;
 
   private connected: boolean;
   // @eslint-kikorp-ne-edituj-sljedecu-liniju
@@ -21,8 +27,12 @@ class Shard {
   private inflate: Inflate;
   private status: ConnectionStatus = 'DISCONNECTED';
 
+  private lastHeartbeat: number;
+  private lastHeartbeatAck: number;
   private heartbeatInterval: ReturnType<typeof setInterval>;
-  private sessionID: number;
+  private reconnectInterval: ReturnType<typeof setInterval>;
+  private resumeGatewayURL: string;
+  private sessionID: number | null;
 
   constructor(client: BaseClient) {
     this.client = client;
@@ -43,12 +53,23 @@ class Shard {
         to: 'string'
       });
     }
-    this.ws = new WebSocket(
-      this.client._gatewayData.url +
-        `/?v=${GatewayVersion}&encoding=json${
+    if (!this.sessionID) {
+      this.ws = new WebSocket(
+        `${this.client._gatewayData.url}/?v=${GatewayVersion}&encoding=json${
           this.client.compress ? '&compress=zlib-stream' : ''
         }`
-    );
+      );
+    } else {
+      // Second url is provided if there isn't a resumeGatewayURL
+      // TODO: dodati warn ako nema resumeGatewayURL, zato sto ce se cesto disconnectovat nakon resuma zbog toga
+      this.ws = new WebSocket(
+        `${
+          this.resumeGatewayURL || this.client._gatewayData.url
+        }/?v=${GatewayVersion}&encoding=json${
+          this.client.compress ? '&compress=zlib-stream' : ''
+        }`
+      );
+    }
     this.ws.on('open', this._onOpen);
     this.ws.on('error', this._onError);
     this.ws.on('message', this._onMessage);
@@ -57,7 +78,7 @@ class Shard {
 
   private _onOpen() {
     this.status = 'HANDSHAKING';
-    console.log('connected');
+    if (this.reconnectInterval) clearInterval(this.reconnectInterval);
   }
   private _onError() {
     console.log('error');
@@ -82,7 +103,22 @@ class Shard {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   private _onClose(code: number, reason: RawData) {
     this.status = 'DISCONNECTED';
-    // TODO: dodati u ivyerror sve codeove, i dodati poruke za njih, ako je reconnectable onda se treba reconnect
+    this.client.emit('disconnect', code, reason);
+    // This is all codes that allows reconnecting to the gateway
+    switch (code) {
+      case GatewayCloseCodes.UnknownError:
+      case GatewayCloseCodes.UnknownOpcode:
+      case GatewayCloseCodes.DecodeError:
+      case GatewayCloseCodes.NotAuthenticated:
+      case GatewayCloseCodes.InvalidSeq:
+      case GatewayCloseCodes.RateLimited:
+      case GatewayCloseCodes.SessionTimedOut:
+        this.reconnect();
+        break;
+      default:
+        this.reconnect();
+        break;
+    }
   }
 
   private handleOPCodes(data: any) {
@@ -92,6 +128,7 @@ class Shard {
         switch (data.t) {
           case 'READY':
             this.sessionID = data.d.session_id;
+            this.resumeGatewayURL = data.d.resume_gateway_url;
             this.status = 'CONNECTED';
             this.client.emit('ready', this.client);
             break;
@@ -100,12 +137,33 @@ class Shard {
       case GatewayOpcodes.Heartbeat:
         this.heartbeat();
         break;
+      case GatewayOpcodes.Reconnect:
+        this.reconnect();
+        break;
+      case GatewayOpcodes.InvalidSession:
+        if (data.d) {
+          this.resume();
+        } else {
+          this.sessionID = null;
+          this.sequence = null;
+          this.reconnect();
+        }
+        break;
       case GatewayOpcodes.Hello:
-        this.heartbeat();
-        this.identify();
-        setInterval(() => this.heartbeat(), data.d.heartbeat_interval);
+        if (this.sessionID) {
+          this.resume();
+        } else {
+          this.heartbeat();
+          this.identify();
+          this.heartbeatInterval = setInterval(
+            () => this.heartbeat(),
+            data.d.heartbeat_interval
+          );
+        }
         break;
       case GatewayOpcodes.HeartbeatAck:
+        this.lastHeartbeatAck = Date.now();
+        this.latency = this.lastHeartbeatAck - this.lastHeartbeat;
         this.client.emit('heartbeat');
         break;
     }
@@ -128,6 +186,36 @@ class Shard {
       })
     );
   }
+  private resume() {
+    if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+      this.status = 'RESUMING';
+      this.ws.send(
+        JSON.stringify({
+          op: 6,
+          d: {
+            token: this.client.token,
+            session_id: this.sessionID,
+            seq: this.sequence
+          }
+        })
+      );
+    }
+  }
+  private reconnect() {
+    if (this.status !== 'DISCONNECTED') return;
+    this.status = 'RECONNECTING';
+    let attempts = this.client.reconnectAttempts ?? Infinity;
+    const reconnectInterval = setInterval(() => {
+      if (attempts === 0) {
+        clearInterval(reconnectInterval);
+        this.status = 'DISCONNECTED';
+        return;
+      }
+      clearInterval(this.heartbeatInterval);
+      this.initWS();
+      attempts--;
+    }, 5000);
+  }
 
   private heartbeat() {
     if (this.ws && this.ws.readyState === WebSocket.OPEN)
@@ -137,6 +225,7 @@ class Shard {
           d: this.sequence
         })
       );
+    this.lastHeartbeat = Date.now();
   }
 }
 
