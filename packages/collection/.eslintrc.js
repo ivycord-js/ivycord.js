a3fef40993d8bd126f90c3bbb91fbfc7eed21cd2
kikorp78 <vukvukovic7@yahoo.com>
2024-01-09 20:27:54 +0100

    chore: add turborepo and extend prettier/eslint files (#6)

--- packages/collection/.eslintrc.js
+++ packages/collection/.eslintrc.js
@@ -1,25 +1,4 @@
+const rootEslintConfig = require('../../.eslintrc.js');
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  ignorePatterns: ['.eslintrc.js', 'dist/**/*'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'no-case-declarations': 'off',
-    'no-inner-declarations': 'off',
-    'arrow-parens': 'warn',
-    semi: 'warn',
-    quotes: ['warn', 'single'],
-    'prefer-const': 'warn',
-    eqeqeq: 'warn',
-    'no-var': 'error',
-    'require-await': 'error'
-  }
+  ...rootEslintConfig
 };
