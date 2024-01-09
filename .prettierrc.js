f0468276ab868ffcbbebfe41844208b80656fd68
Vuk Vukovic <vukvukovic7@yahoo.com>
2024-01-09 20:17:17 +0100

    chore: add turborepo and extend prettier/eslint files

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: 'always',
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true
+};
--- examples/test.js
+++ examples/test.js
@@ -1,16 +1,16 @@
-const { BaseClient } = require("../packages/ivycord.js/dist/index");
+const { BaseClient } = require('../packages/ivycord.js/dist/index');
 
 const client = new BaseClient({
-  token: "YOUR_TOKEN",
-  compress: true,
+  token: 'YOUR_TOKEN',
+  compress: true
 });
 
-client.on("ready", () => {
-  console.log("ready, latency " + client.shard.latency);
+client.on('ready', () => {
+  console.log('ready, latency ' + client.shard.latency);
 });
 
-client.on("heartbeat", () => {
-  console.log("heartbeat received");
+client.on('heartbeat', () => {
+  console.log('heartbeat received');
 });
 
 client.connect();
