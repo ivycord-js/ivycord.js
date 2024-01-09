532e40a17e0ba38130c511d2bae8c9086d90791e
Vuk Vukovic <vukvukovic7@yahoo.com>
2024-01-09 18:14:47 +0100

    chore: add *.yaml files to .prettierignore files

--- examples/test.js
+++ examples/test.js
@@ -1,16 +1,16 @@
 const { BaseClient } = require("../packages/ivycord.js/dist/index");
 
 const client = new BaseClient({
-  token:
-    "MTE1MzA1MDg2NjkzMzMwOTUwMA.GKn0YM.5nlA4n2Q6kICBPIe-kjk5Vt2yxj-CCHf2n5keY",
+  token: "YOUR_TOKEN",
   compress: true,
-});
-client.on("heartbeat", () => {
-  console.log("heartbeat received");
 });
 
 client.on("ready", () => {
   console.log("ready, latency " + client.shard.latency);
+});
+
+client.on("heartbeat", () => {
+  console.log("heartbeat received");
 });
 
 client.connect();
