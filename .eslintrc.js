d9708800e7e59fca60105b79c69a46671817da7a
kikorp78 <vukvukovic7@yahoo.com>
2024-01-13 21:08:33 +0100

    chore(eslint): update config (#10)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,24 +5,32 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json'
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
   },
-  plugins: ['@typescript-eslint', 'unused-imports'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
+  plugins: ['@typescript-eslint', 'unused-imports', 'eslint-comments'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['.eslintrc.js', 'dist/**/*'],
   rules: {
+    // Default rules
+    'arrow-parens': 'warn',
+    'prefer-const': 'warn',
+    eqeqeq: 'warn',
+    'no-var': 'error',
+
+    // Typescript rules
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/semi': 'warn',
+    '@typescript-eslint/quotes': ['warn', 'single'],
+    '@typescript-eslint/require-await': 'error',
     '@typescript-eslint/await-thenable': 'error',
     '@typescript-eslint/no-floating-promises': 'error',
-    'no-case-declarations': 'off',
-    'no-inner-declarations': 'off',
+    '@typescript-eslint/no-empty-function': 'error',
+
+    // Third-party plugin rules
+    'eslint-comments/no-unused-disable': 'warn',
     'unused-imports/no-unused-vars': [
       'warn',
       {
@@ -32,13 +40,6 @@ module.exports = {
         argsIgnorePattern: '^_'
       }
     ],
-    'arrow-parens': 'warn',
-    semi: 'warn',
-    quotes: ['warn', 'single'],
-    'prefer-const': 'warn',
-    eqeqeq: 'warn',
-    'no-var': 'error',
-    'require-await': 'error',
     'unused-imports/no-unused-imports': 'error'
   }
 };
