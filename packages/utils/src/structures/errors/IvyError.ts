44fe08d6c36cabacdfadb10974b90245a19a8605
null68 <fariscomaga@gmail.com>
2024-01-13 22:22:42 +0100

    Fix formatting of EVENT_NOT_IMPLEMENTED message

--- packages/utils/src/structures/errors/IvyError.ts
+++ packages/utils/src/structures/errors/IvyError.ts
@@ -3,7 +3,7 @@ const messages = {
   LARGE_THRESHOLD_INVALID: 'Large threshold must be between 50 and 250',
   EVENTS_LOAD_FAILED:
     'Failed to load events. Try reinstalling the package or contact the developer.',
-  EVENT_NOT_IMPLEMENTED: '{message} event isn\'t implemented yet.',
+  EVENT_NOT_IMPLEMENTED: "{message} event isn't implemented yet.",
   // Gateway
   WS_ALREADY_CONNECTED: 'Already connected to the WebSocket',
   SHARD_COUNT_SHARDS_START_FROM_MISMATCH:
